3dcfd2a0e7d3f9d79b007edaadfc11e2bbe9c783
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2022-09-12 00:01:50 -0700

    useState example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
   const [counter,setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100);
+  }, [])
+
   return (
     <div className="App">
       <button onClick={() => setCounter((prevCount) => prevCount - 1 )}>-</button>
