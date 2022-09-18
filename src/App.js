817741dadab3614296ba8ee6e6c4da7607a8bfc1
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2022-09-18 11:16:59 -0700

    Project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import MovieCardContainer from "./MovieCardContainer";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=35465dbe";
 
 const App = () => {
-  const [counter,setCounter] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    setCounter(100);
-  }, [])
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
 
   return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1 )}>-</button>
-      <h2>{counter}</h2>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCardContainer movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
